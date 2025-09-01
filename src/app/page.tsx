55770e90a999d817da8f66892bd6f44b8ec8867d
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-09-01 18:11:03 -0300

    chore(global.css): variables de colores para el proyecto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hiro from "@/components/landing/Hiro";
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen text-4xl text-center">
-      levantando la app desde Docker
+      <Hiro />
     </div>
   );
 }
