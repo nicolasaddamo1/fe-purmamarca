0a3989e6e603f2431271d438ff400fb3a0efacbc
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-09-03 18:20:44 -0300

    feat(StarProduct): Se agrego texto promocional

--- src/components/landing/Hero/StarProduct.tsx
+++ src/components/landing/Hero/StarProduct.tsx
@@ -2,9 +2,12 @@ const StarProduct = () => {
   return (
     <section className="py-16">
       <div className="mx-auto px-4 text-center container">
-        <h2 className="mb-8 text-primary text-3xl md:text-4xl">
+        <h3 className="mb-4 text-tertiary text-xl">
           Nuestros productos Estrellas
-        </h2>
+        </h3>
+        <p className="mx-auto mb-8 max-w-xl text-primary text-2xl md:text-3xl">
+          Productos seleccionados con la mejor calidad y cercanía para vos.
+        </p>
 
         {/*TODO Acá después van las cards o el carrusel */}
         <div className="flex justify-center items-center text-gray-500">
