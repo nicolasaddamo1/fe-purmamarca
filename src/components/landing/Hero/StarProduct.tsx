bec4abc7dae21dcce60af4e6ab64829f03803709
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-09-03 15:13:38 -0300

    feat: crear componente star-product

---
+++ src/components/landing/Hero/StarProduct.tsx
@@ -0,0 +1,18 @@
+const StarProduct = () => {
+  return (
+    <section className="py-16">
+      <div className="mx-auto px-4 text-center container">
+        <h2 className="mb-8 text-primary text-3xl md:text-4xl">
+          Nuestros productos Estrellas
+        </h2>
+
+        {/*TODO Acá después van las cards o el carrusel */}
+        <div className="flex justify-center items-center text-gray-500">
+          <p className="italic"> Destacados Acá...</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default StarProduct;
