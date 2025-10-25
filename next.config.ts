3198067e85826ad2e5eb30277b88b237fa1805b7
dylan correa <D.correa.E05@gmail.com>
2025-10-25 16:10:05 -0300

    chore: images updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,47 @@ import type { NextConfig } from "next";
 import withFlowbiteReact from "flowbite-react/plugin/nextjs";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'pbs.twimg.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'dcdn-us.mitiendanube.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'example.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'encrypted-tbn0.gstatic.com',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'd22fxaf9t8d39k.cloudfront.net',
+                port: '',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'acdn-us.mitiendanube.com',
+                port: '',
+                pathname: '/**',
+            },
+           
+        ],
+    },
 };
 
 export default withFlowbiteReact(nextConfig);
