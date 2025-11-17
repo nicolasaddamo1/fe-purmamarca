7421a8b352b90d6ea26a4f76f175a90736bfc43b
Mauricio Vllgs <140904662+V-Mau@users.noreply.github.com>
2025-11-17 16:18:45 -0300

    feat(about-us): agregamos galeria de productos aleatorios

--- src/app/(site)/aboutUs/page.tsx
+++ src/app/(site)/aboutUs/page.tsx
@@ -1,18 +1,16 @@
 import QualitiesSection from "@/components/landing/Features/FeaturesSection";
+import GalleryProducts from "@/components/landing/GalleryProducts/GalleryProducts";
 import AboutUs from "@/components/landing/Hero/AboutUs";
 import Hero from "@/components/landing/Hero/Hero";
-// import StarProduct from "@/components/landing/Hero/StarProduct";
-// import ProductsSection from "@/components/landing/Products/ProductsSection";
-import TestimonialsSection from "@/components/landing/Testimonials/TestimonialSection";
 
 export default function Home() {
   return (
     <div>
       <Hero />
-      {/* <StarProduct /> */}
+
       <AboutUs />
-      {/* <ProductsSection /> */}
-      <TestimonialsSection />
+
+      <GalleryProducts />
       <QualitiesSection />
     </div>
   );
